418aa022252cde05d1e081ee360593b1538821d7
rio45ka <rio.aska35@gmail.com>
2018-11-11 12:26:40 +0700

    init xcode project

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Rio on 11/11/18.
+//  Copyright © 2018 Aurora. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
